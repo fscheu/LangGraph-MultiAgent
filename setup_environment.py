25e912fd197347b0185c1cbdada616d8d8d1adb6
Federico Scheu <fscheu@gmail.com>
2024-11-19 10:32:29 -0300

    Basic LangChain and LangSmith setup

--- setup_environment.py
+++ setup_environment.py
@@ -11,5 +11,14 @@ def read_ini(file_path):
 
 config = read_ini("config.ini")
 
-def set_environment_variables() -> None:
+
+def set_environment_variables(project_name: str = "") -> None:
     os.environ["OPENAI_API_KEY"] = str(config["LLM"]["OPENAI_API_KEY"])
+
+    if not project_name:
+        project_name = f"Test_{date.today()}"
+
+    os.environ["LANGCHAIN_TRACING_V2"] = str(config["LANG"]["LANGCHAIN_TRACING_V2"])
+    os.environ["LANGCHAIN_API_KEY"] = str(config["LANG"]["LANGCHAIN_API_KEY"])
+    os.environ["LANGCHAIN_PROJECT"] = project_name
+    print("API Keys loaded and tracing set with project name: ", project_name)
